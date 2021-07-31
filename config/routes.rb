0f6139ac6ac59d0bd8a885107aeb0f50e78c41da
Dex <fitch.dexter@gmail.com>
2021-07-31 01:33:47 -0700

    Build out basic nav and main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :entries, only: [:index, :show]
+  get '/work', to: 'entries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#home'
 end
